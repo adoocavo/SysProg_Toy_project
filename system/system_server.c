d5408c40e2e04000ab1fc86936118e13377f046a
adoocavo <paulckj123@gmail.com>
2023-11-30 22:43:28 +0900

    06_timer : system_server.c에 posix timer 구현

--- system/system_server.c
+++ system/system_server.c
@@ -3,11 +3,96 @@
 #include <sys/unistd.h>    //for getpid() syscall
 #include <sys/types.h>     //for getpid() syscall
 #include <sys/wait.h>      //for waitpid() syscall  
+#include <sys/time.h>
+#include <time.h>
+#include <signal.h>
+#include <unistd.h>
+//#include <bits/sigevent-consts.h>    //for SIGEV_SIGNAL
+//#include <bits/sigaction.h>
 
 #include "system_server.h"
 #include "./../ui/gui.h"
 #include "./../ui/input.h"
 #include "./../web_server/web_server.h"
+#include "./../project_libs/time/currTime.h"
+
+#define TIMER_SIG SIGRTMAX      //POSIX RTS 사용
+
+
+/** feature : main process가 생성한 모든 process monitoring 
+ * @param {long} initial_sec, initial_usec, interval_sec, interval_usec
+ * @return {void} 
+ * @todo  posix timer set + create (by using timer_create() and timer_create())
+*/
+void set_create_peridicTimer(long initial_sec, long initial_usec, long interval_sec, long interval_usec)
+{
+    timer_t *tidlist;
+
+    //1. sigevent struct 설정 for timer_create() + timer_create()
+    struct sigevent   sev;    
+    sev.sigev_notify = SIGEV_SIGNAL;
+    sev.sigev_signo = TIMER_SIG;
+
+    if(timer_create(CLOCK_REALTIME, &sev, &tidlist[0]) == -1)  perror("timer_create");
+
+
+    //2. itimerspec struct 설정 for timer_settime() + timer_settime()
+    struct itimerspec ts;
+
+    //2_1. timer 시간 간격, 초기값 설정
+    ///it_value : 첫 만기 시점(첫 번째 TIMER_SIG 발생 시점)
+    ts.it_value.tv_sec = initial_sec;              //sec 값
+    ts.it_value.tv_nsec = initial_usec;             //nano sec 값
+
+    ///it_interval : 타이머의 반복 주기()
+    ts.it_interval.tv_sec = interval_sec;         
+    ts.it_interval.tv_nsec = interval_usec;
+
+    //2_2. timer_settime
+    if(timer_settime(tidlist[0], 0, &ts, NULL) == -1)  perror("timer_settime");
+
+}
+
+
+
+
+/**
+ * @note Set nonzero on receipt of SIGALRM : SIGALRM -> toy_timer = 1;
+*/
+//static int toy_timer = 0;  
+/*
+int posix_sleep_ms(unsigned int timeout_ms)
+{
+    struct timespec sleep_time;
+
+    sleep_time.tv_sec = timeout_ms / MILLISEC_PER_SECOND;
+    sleep_time.tv_nsec = (timeout_ms % MILLISEC_PER_SECOND) * (NANOSEC_PER_USEC * USEC_PER_MILLISEC);
+
+    return nanosleep(&sleep_time, NULL);
+}
+*/
+
+/** feature : TIMER_SIG handler 
+ * @param {void} 
+ * @return {int} 0
+ * @todo : tick값() 출력 
+*/
+static void timerSig_handler(int sig, siginfo_t *si, void *uc)
+{
+
+    timer_t *tidptr;
+
+    tidptr = si->si_value.sival_ptr;
+
+    /* UNSAFE: This handler uses non-async-signal-safe functions
+       (printf(); see Section 21.1.2) */
+
+    printf("[%s] Got signal %d\n", currTime("%T"), sig);
+    printf("    *sival_ptr         = %ld\n", (long) *tidptr);
+    printf("    timer_getoverrun() = %d\n", timer_getoverrun(*tidptr));
+
+}
+
 
 
 /** feature : main process가 생성한 모든 process monitoring 
@@ -17,12 +102,33 @@
 */
 int system_server()
 {
+//    struct itimerspec ts;
+    struct sigaction  sa;
+//    struct sigevent   sev;
+    timer_t *tidlist;
+
     printf("나 system_server 프로세스!\n");
-    //printf("system_server process start!!\n");
+    
+    /* 5초 타이머를 만들어 봅시다. */
+
+    //1. sig hanlder 등록(TIMER_SIG)
+    memset(&sa, 0, sizeof(sigaction));
+
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_SIGINFO;
+    sa.sa_sigaction = timerSig_handler;
+
+    if(sigaction(TIMER_SIG, &sa, NULL) == -1)   perror("sigaction");
+
+    //2. set + create timer
+    set_create_peridicTimer(5, 0, 5, 0);
+
+    printf("system init done.  waiting...");
 
     while (1) 
     {
-        sleep(1);
+        sleep(5);
+        //posix_sleep_ms(5000);
     }
     
     return 0;
